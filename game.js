216e2303768f77b1fedb60e1130b8217fec9c89a
Oto-obong Effiong <effiongoto98@gmail.com>
2020-08-28 08:58:28 +0100

    Initial files for simon-game

---
+++ game.js
@@ -0,0 +1,95 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+// variable to keep track of whether if the game has started or not
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+
+    //The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function(){
+
+  var userChosenColour = $(this).attr('id');
+  userClickedPattern.push(userChosenColour);
+  //console.log(userClickedPattern);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1)
+});
+
+function checkAnswer(currentLevel) {
+
+    //Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+      //If the user got the most recent answer right, then check that they have finished their sequence with another if statement.
+      if (userClickedPattern.length === gamePattern.length){
+
+        //Call nextSequence() after a 1000 millisecond delay.
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+
+    } else {
+
+      console.log("wrong");
+      var wrong = new Audio("sounds/wrong.mp3");
+      wrong.play();
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver()
+    }
+
+}
+
+function nextSequence() {
+
+    //Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level)
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+
+
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+function startOver() {
+  //reset the values of level, gamePattern and started variables.
+  level = 0;
+  gamePattern = [];
+  started = false;
+
+}
